1504a75382e18ac5b964680e36641458fbce07e8
Ibukun Akinbami <ibukunakinbami@gmail.com>
2022-06-16 13:31:25 +0100

    Updated Appjs

--- reactpractise/src/App.js
+++ reactpractise/src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Sensei from './component/Person';
 
-
-function app() {
+//FUNCTION BASED COMPONENT
+/*
+function App() {
   return (
     <div className="App">
       <h1>HI, I AM A REACT DEVELOPER</h1>
@@ -15,5 +16,31 @@ function app() {
     </div>
   );
 }
+*/
 
-export default app;
+//CLASS BASED COMPONENT
+class App extends Component() {
+  
+  state = {
+    precord:[
+      {name: "SEGUN", age: 45},
+      {name: "DEOLA", age: 78},
+      {name: "  KEJI", age: 34}
+    ]
+  }
+
+  render(){
+  return (
+    <div className="App">
+      <h1>HI, I AM A REACT DEVELOPER</h1>
+      <Sensei name = {this.state.precord[0].name} age={this.state.precord[0].age} />
+      <Sensei name = {this.state.precord[1].name} age={this.state.precord[1].age} />
+      <Sensei name = {this.state.precord[2].name} age={this.state.precord[2].age} />
+
+      <button>SWITCH</button>
+    </div>
+  );
+  }
+}
+
+export default App;
